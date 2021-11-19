1c0b3cd9c6da482fa9df3d44dda8682e41d2d6e9
Everett1914 <qwest1914@gmail.com>
2021-11-18 22:23:23 -0500

    Updating db links

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
 const express = require("express");
+const cors = require("cors");
+const { pool } = require("./config");
+
 const app = express();
 const importData = require("./data.json");
 let port = process.env.PORT || 3000;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors());
 
 app.get("/", (req, res) => {
     res.send("benoit-movie-tracker-api");
@@ -10,6 +17,19 @@ app.get("/", (req, res) => {
 app.get("/players", (req, res) => {
     res.send(importData);
 });
+
+const getShows = (request, response) => {
+    pool.query('SELECT * FROM shows', (error, results) => {
+        if (error) {
+            throw error
+        }
+        response.status(200).json(results.rows)
+    })
+}
+
+app
+    .route('/shows')
+    .get(getShows)
 
 app.listen(port, () => {
     console.log(`App is listening on port http://localhost:${port}`);
