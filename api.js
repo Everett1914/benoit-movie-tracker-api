317c0e8dce77b9b3ddc46b35b37d51feb5bcd1f9
Everett1914 <qwest1914@gmail.com>
2021-11-20 18:28:57 -0500

    Created Fix for ssl, may need to update for more security later

--- api.js
+++ api.js
@@ -4,7 +4,8 @@ const pool = new Pool({
   host:'ec2-3-228-134-188.compute-1.amazonaws.com',
   database:'dcr488381q6u5b',
   password:'8bb9d700188814f90f41e9b3a40a0a46d9224e46c5aeb442289c1d7eb046b395',
-  port:5432
+  port:5432,
+  ssl: { rejectUnauthorized:false }
 })
 
 const getAllShows = async (request, response) => {
