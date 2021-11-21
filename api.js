dbcafbf396a44deaffae38abebb6a24ace778e75
Everett1914 <qwest1914@gmail.com>
2021-11-20 23:40:06 -0500

    Updated api to use environment variables

--- api.js
+++ api.js
@@ -6,7 +6,7 @@ const pool = new Pool({
   database:process.env.DB_DATABASE,
   password:process.env.DB_PASSWORD,
   port:process.env.DB_PORT,
-  ssl:JSON.process.env.DB_SSL
+  ssl:process.env.DB_SSL
   //ssl: { rejectUnauthorized:false }
 });
 
