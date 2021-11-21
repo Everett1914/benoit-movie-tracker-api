5ce65162739d261a7e772abd599ab2c886ccb099
Everett1914 <qwest1914@gmail.com>
2021-11-20 21:02:28 -0500

    Updated api to use environment variables

--- api.js
+++ api.js
@@ -1,11 +1,11 @@
 require('dotenv').config()
 const Pool = require('pg').Pool;
 const pool = new Pool({
-  user:process.env.DB_USER,
-  host:process.env.DB_HOST,
-  database:process.env.DB_DATABASE,
-  password:process.env.DB_PASSWORD,
-  port:process.env.DB_PORT,
+  user:'cqdzknyukdxjih',
+  host:'ec2-3-228-134-188.compute-1.amazonaws.com',
+  database:'dcr488381q6u5b',
+  password:'8bb9d700188814f90f41e9b3a40a0a46d9224e46c5aeb442289c1d7eb046b395',
+  port:5432,
   ssl: { rejectUnauthorized:false }
 })
 
