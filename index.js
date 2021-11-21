c6b5e0dc73acd3102bc3e748498cdd74c6d9fee0
Everett1914 <qwest1914@gmail.com>
2021-11-20 23:17:33 -0500

    Updated api to use environment variables

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
-    res.send("benoit-movie-tracker-api test");
+    res.send("benoit-movie-tracker-api");
 });
 
 app.get('/shows', db.getAllShows);
